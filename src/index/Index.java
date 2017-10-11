3054189352b34105504c6f613582be87ab0555cb
Lucas Henrique <lucashenrique580@gmail.com>
2017-10-10 23:45:59 -0300

    inverted index e mesure index prontos (nao testados)

--- src/index/Index.java
+++ src/index/Index.java
@@ -12,31 +12,31 @@ import util.FileManip;
 
 
 public class Index {
+	JCL_facade jcl = JCL_FacadeImpl.getInstance();
+
 	// cria arquivos metadata em todas as maquinas do cluster
-		public void metaData()
-		{
-			String mesure = null, dimension = null;
-			try {
+	public void metaData()
+	{
+		String mesure = null, dimension = null;
+		try {
 			// le asrquivos metadata
-				mesure = FileManip.metaDataToString("input/mesuresnames.mg");
-				dimension = FileManip.metaDataToString("input/dimensionsnames.mg");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			JCL_facade jcl = JCL_FacadeImpl.getInstance();
-			jcl.register(FileManip.class, "fileManip");
-			Object [] args = {mesure, dimension};
-		// escreve os arquivos metadata em todas as maquinas do cluster
-			jcl.executeAll("fileManip", "writeMetaData", args);
-		// cria as hashMaps com os metadados para cada maquina
-			jcl.executeAll("fileManip", "readMetaData", null);
+			mesure = FileManip.metaDataToString("input/mesuresnames.mg");
+			dimension = FileManip.metaDataToString("input/dimensionsnames.mg");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
-	public static void main(String [] args) {
-		JCL_facade jcl = JCL_FacadeImpl.getInstance();
-		jcl.register(Index.class, "Index");
+
+		jcl.register(FileManip.class, "fileManip");
+		Object [] args = {mesure, dimension};
+		// escreve os arquivos metadata em todas as maquinas do cluster
+		jcl.executeAll("fileManip", "writeMetaData", args);
+		// cria as hashMaps com os metadados para cada maquina
+		jcl.executeAll("fileManip", "readMetaData", null);
+	}
+
+	public void createIndex(){
+		jcl.register(FileManip.class, "Index");
 		Map<Entry<String,String>,Integer> hosts = jcl.getAllDevicesCores();
 		Object [][] arg = new Object[hosts.size()][];
 		int i = 0;
@@ -45,10 +45,14 @@ public class Index {
 				Object [] a = {i,j};
 				arg[i][j] = a; 
 			}
-			 
 		}
-		
+
 		jcl.executeAllCores("Index", "createIndex", arg);
 	}
-	
+
+	public static void main(String [] args) {
+
+
+	}
+
 }
--- src/util/FileManip.java
+++ src/util/FileManip.java
@@ -14,9 +14,14 @@ import java.io.ObjectOutputStream;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import it.unimi.dsi.fastutil.ints.Int2DoubleOpenHashMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntCollection;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import implementations.collections.JCLHashMap;
+import implementations.dm_kernel.user.JCL_FacadeImpl;
 
 public class FileManip {
 
@@ -93,83 +98,159 @@ public class FileManip {
 		}
 	}
 	
-	// Le os metadados que estão na maquina especifica
-		public static void readMetaData() throws IOException
+// funcao do index
+	public static void readIndex(int machineID, int fileID) throws IOException
+	{
+		// map dos metadados
+		Map<String, Integer> mesureMeta = JCL_FacadeImpl.GetHashMap("Mesure");
+		Map<String, Integer> dimensionMeta = JCL_FacadeImpl.GetHashMap("Dimesion");
+		
+		int qtdMesure = mesureMeta.size();
+		
+		String line = null;
+		FileReader f = null;
+		try
 		{
-			String line = null;
-			FileReader f1 = null;
-			FileReader f2 = null;
-			try
-			{
-				f1 = new FileReader("mesurenames.mg");
-				f2 = new FileReader("dimensionsnames.mg");
-			}
-			catch(FileNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-			BufferedReader reader1 = new BufferedReader(f1);
-			BufferedReader reader2 = new BufferedReader(f2);
-			Map<String, Integer> mesure = new JCLHashMap<>("Mesure");
-			Map<String, Integer> dimension = new JCLHashMap<>("Dimension");
-			while((line = reader1.readLine()) != null)
-			{
-				String [] lineArr = line.split("=");
-				mesure.put((lineArr[1]), Integer.parseInt(lineArr[0]));
-			}
-			while((line = reader2.readLine()) != null)
-			{
-				String [] lineArr = line.split("=");
-				dimension.put((lineArr[1]), Integer.parseInt(lineArr[0]));
-			}
-			f1.close();
-			f2.close();
+			f = new FileReader("arq_"+fileID+".txt");
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
 		}
 		
-	// Escreve os metadados em todas as maquinas do cluster
-		public static void writeMetaData(String mesure, String dimensions) throws IOException
-		{
-			FileWriter f1 = null;
-			FileWriter f2 = null;
-			try
-			{
-				f1 = new FileWriter("mesurenames.mg");
-				f2 = new FileWriter("dimensionsnames.mg");
-			}
-			catch(FileNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-			BufferedWriter writer1 = new BufferedWriter(f1);
-			BufferedWriter writer2 = new BufferedWriter(f2);
-			writer1.write(mesure);
-			writer2.write(dimensions);
-			f1.close();
-			f2.close();
-		}
+		// map dos indices invertidos
+		Object2ObjectOpenHashMap<String, IntCollection> invertedIndex = new Object2ObjectOpenHashMap<String, IntCollection>();
+		// map do jcl para dar putAll
+		Map<String, IntCollection> jclInvertedIndex = new JCLHashMap<String, IntCollection>("invertedIndex");
+
+		// map dos mesure index
+		Object2ObjectOpenHashMap<String, Int2DoubleOpenHashMap> mesureIndex = new Object2ObjectOpenHashMap<String, Int2DoubleOpenHashMap>(); 
+		// map do jcl para dar putAll
+		Map<String, Int2DoubleOpenHashMap> jclMesureIndex = new JCLHashMap<String, Int2DoubleOpenHashMap>("mesureIndex"); 
 		
-	// le o arquivo metaData pela primeira vez
-		public static String metaDataToString(String nome) throws IOException
+		BufferedReader reader = new BufferedReader(f);
+		reader.readLine();
+		// le linha a linha do arquivo
+		while((line = reader.readLine()) != null)
 		{
-			String line = null;
-			FileReader f = null;
-			try
-			{
-				f = new FileReader(nome);
-			}
-			catch(FileNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-			StringBuffer b = new StringBuffer();
-			BufferedReader reader = new BufferedReader(f);
-			while((line = reader.readLine()) != null)
-			{
-				b.append(line+'\n');
-			}
-			f.close();
-			return b.toString();
+			// separa a tupla
+			String [] splitArr = line.split("\\|");
 			
+			// valor de onde a coluna das dimensions comeca. Aqui pegamos a quantidade de colunas de mesure + 1(coluna das PK)
+			int col = qtdMesure + 1;
+			// rodamos a quantidade de colunas das dimensions
+			for(int i=0;i<dimensionMeta.size();i++) {
+				// pega o conteudo da coluna
+				String d = splitArr[col++];
+				// verifica se este conteudo ja existe na minha map(no caso, o conteudo e' a chave da map)
+				if(invertedIndex.get(d) == null) {
+					// cria uma lista que guarda as PK onde houve a ocorrencia de d(chave da map)
+					IntArrayList dimensionList = new IntArrayList();
+					// guarda na map de indice invertido a chave d e a lista vazia
+					invertedIndex.put(d, dimensionList);
+				}
+				// pesquisa pela chave d, caso exista, adiciono mais um PK a lista
+				invertedIndex.get(d).add(Integer.parseInt(splitArr[0]));
+			}
+			
+			// rodamos a quantidade de colunas da mesure
+			for(int i = 1; i <= qtdMesure; i++)
+			{
+			// pega o conteudo da coluna
+				String m = splitArr[i];	
+			// verifica se a map de mesures possui a chave (que nesse caso e' nossa PK)
+				if(mesureIndex.get(splitArr[0]) == null)
+				{
+				// cria uma map int para double
+					Int2DoubleOpenHashMap mesureMap = new Int2DoubleOpenHashMap();
+				// adiciona a map das mesures a chave (PK) e a map vazia que acabou de ser criada
+					mesureIndex.put(splitArr[0], mesureMap);
+				}
+			// Pesquisa pela chave PK, caso exista, adiciona os valores para a map int2double e adiciona ela para a map principal
+				mesureIndex.get(splitArr[0]).put(i, Double.parseDouble(m));			
+			}
 		}
+		jclInvertedIndex.putAll(invertedIndex);
+		jclMesureIndex.putAll(mesureIndex);
+	}
+
+// Le os metadados que estão na maquina especifica
+	public static void readMetaData() throws IOException
+	{
+		String line = null;
+		FileReader f1 = null;
+		FileReader f2 = null;
+		try
+		{
+			f1 = new FileReader("mesurenames.mg");
+			f2 = new FileReader("dimensionsnames.mg");
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		BufferedReader reader1 = new BufferedReader(f1);
+		BufferedReader reader2 = new BufferedReader(f2);
+		Map<String, Integer> mesure = new JCLHashMap<>("Mesure");
+		Map<String, Integer> dimension = new JCLHashMap<>("Dimension");
+		while((line = reader1.readLine()) != null)
+		{
+			String [] lineArr = line.split("=");
+			mesure.put((lineArr[1]), Integer.parseInt(lineArr[0]));
+		}
+		while((line = reader2.readLine()) != null)
+		{
+			String [] lineArr = line.split("=");
+			dimension.put((lineArr[1]), Integer.parseInt(lineArr[0]));
+		}
+		f1.close();
+		f2.close();
+	}
+	
+// Escreve os metadados em todas as maquinas do cluster
+	public static void writeMetaData(String mesure, String dimensions) throws IOException
+	{
+		FileWriter f1 = null;
+		FileWriter f2 = null;
+		try
+		{
+			f1 = new FileWriter("mesurenames.mg");
+			f2 = new FileWriter("dimensionsnames.mg");
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		BufferedWriter writer1 = new BufferedWriter(f1);
+		BufferedWriter writer2 = new BufferedWriter(f2);
+		writer1.write(mesure);
+		writer2.write(dimensions);
+		f1.close();
+		f2.close();
+	}
+	
+// le o arquivo metaData pela primeira vez
+	public static String metaDataToString(String nome) throws IOException
+	{
+		String line = null;
+		FileReader f = null;
+		try
+		{
+			f = new FileReader(nome);
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		StringBuffer b = new StringBuffer();
+		BufferedReader reader = new BufferedReader(f);
+		while((line = reader.readLine()) != null)
+		{
+			b.append(line+'\n');
+		}
+		f.close();
+		return b.toString();
 		
+	}
+	
 }
