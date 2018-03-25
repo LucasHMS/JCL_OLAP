8ab377c5375dd001649f29e9b3489899db81cb61
GabrielNathan7 <gabrielnata07@gmail.com>
2018-03-25 16:52:53 -0300

    Operadores relacionais arrumados

--- src/query/filter/operators/EqualsToOp.java
+++ src/query/filter/operators/EqualsToOp.java
@@ -5,10 +5,21 @@ public class EqualsToOp implements FilterOperator {
 	@Override
 	public boolean op(String record, String usrArg) {
     	boolean x = false;
-    	float d = Float.parseFloat(record);
-    	float p = Float.parseFloat(usrArg);
-    	if(d == p){
-    		x = true;
+    	if(record.contains("[0-9]+") && usrArg.contains("[0-9]+"))
+    	{
+    		float d = Float.parseFloat(record);
+        	float p = Float.parseFloat(usrArg);
+        	if(d == p){
+        		x = true;
+        	}
+    	}
+    	else
+    	{
+    		int i = record.compareTo(usrArg);
+    		if(i==0)
+    		{
+        		x = true;
+        	}
     	}
     	
     	return x;
