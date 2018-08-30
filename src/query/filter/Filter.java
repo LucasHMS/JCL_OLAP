6b450d8484fa7d021eca9aaf7c31a039aceff60b
Lucas Henrique <lucashenrique580@gmail.com>
2018-08-30 20:28:08 -0300

    adding cardinality calculation and using the new inverted index on the filtering

--- src/query/filter/Filter.java
+++ src/query/filter/Filter.java
@@ -1,9 +1,11 @@
 package query.filter;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import implementations.dm_kernel.user.JCL_FacadeImpl;
 import interfaces.kernel.JCL_facade;
@@ -20,7 +22,7 @@ public class Filter
 	private JCL_facade jcl;
 	private Map<String, Integer> mesureMeta;
 	private Map<String, Integer> dimensionMeta;
-	private Object2ObjectMap<String, IntCollection> jclInvertedIndex;
+//	private Object2ObjectMap<String, IntCollection> jclInvertedIndex;
 	
 	public Filter() {
     	jcl = JCL_FacadeImpl.getInstance();
@@ -45,53 +47,88 @@ public class Filter
 		System.out.println("Iniciando Filter. core " + coreID);
 
 		Int2ObjectMap<String> map_core = (Int2ObjectMap<String>) jcl.getValue(machineID+"_core_"+coreID).getCorrectResult();
-		jclInvertedIndex =  (Object2ObjectMap<String, IntCollection>) jcl.getValue(machineID+"_invertedIndex_"+coreID).getCorrectResult();
+		Object2ObjectMap<String, IntCollection> jclInvertedIndex =  (Object2ObjectMap<String, IntCollection>) 
+																	jcl.getValue(machineID+"_invertedIndex_"+coreID).getCorrectResult();
 
-		List<String> inquireCols = new ArrayList<>();
+	  /*List<String> inquireCols = new ArrayList<>();
 		while(operators.contains(7)) {
 			operators.remove(operators.size()-1);
 			String col = columns.remove(columns.size()-1);
 			inquireCols.add(col);
-		}
-
-		// lista para salvar as maps de resultado de cada filtro da consulta
-		Int2ObjectMap<String> filterResults = new Int2ObjectOpenHashMap<>();
-		filterResults = runFilter(map_core, columns.get(0), operators.get(0), args.get(0));
+		}*/
 		
-		for (int i=0; i<columns.size(); i++) {
+		// lista para salvar as maps de resultado de cada filtro da consulta
+		
+		int cubeDim = 0;
+		
+		Int2ObjectMap<String> filterResults = new Int2ObjectOpenHashMap<>();
+		Object [] returns = runFilter(map_core, jclInvertedIndex, columns.get(0), operators.get(0), args.get(0));
+		filterResults = (Int2ObjectMap<String>) returns[0];
+		cubeDim *= (new Integer(returns[1].toString()));
+		
+		for (int i=0; i<operators.size()&&operators.get(i)!=7; i++) {
 			Int2ObjectMap<String> aux = new Int2ObjectOpenHashMap<String>();
-			aux = runFilter(filterResults, columns.get(i), operators.get(i), args.get(i));
-			
+			Object [] returns1 = runFilter(filterResults, jclInvertedIndex, columns.get(i), operators.get(i), args.get(i));
+			aux = (Int2ObjectMap<String>) returns1[0];
+			cubeDim *= (new Integer(returns1[1].toString()));
+
+			returns1[0] = null;
+			returns1[1] = null;
 			filterResults.clear();
 			filterResults = null;
 			filterResults = new Int2ObjectOpenHashMap<>(aux);
 		}
 		
-		if(inquireCols.size() > 0) {
+		/*if(inquireCols.size() > 0) {
 			columns.addAll(inquireCols);
+		}*/
+		
+		if(operators.indexOf(7) >= 0) {
+			System.out.println("Calculando a cardinalidade INQUIRE. core " + coreID);		
+			for(int i=operators.indexOf(7);i<operators.size();i++) {
+				int card = cardilanityInquire(map_core, jclInvertedIndex.keySet(), columns.get(i));
+				cubeDim *= card;
+			}
 		}
+		
+		cubeDim += 1;
 		
 		Object2ObjectMap<String, IntCollection> cleanResult = generateReult(filterResults, columns);
 		
 		jcl.deleteGlobalVar(machineID+"_core_"+coreID);
 		map_core = null;
+		
 		jcl.instantiateGlobalVar(machineID+"_filter_core_"+coreID, cleanResult);
+		jcl.instantiateGlobalVar(machineID+"_cubeDim_"+coreID, cubeDim);
+		
+		System.out.println(machineID+"_cubeDim_"+coreID+": " + cubeDim);
 		System.out.println("Finalizou a Filtragem. core " + coreID);
     }
     
-    private Int2ObjectMap<String> runFilter(Int2ObjectMap<String> tuples, String column, int opID, String arg) {
+    private /*Int2ObjectMap<String>*/Object [] runFilter(Int2ObjectMap<String> tuples, Object2ObjectMap<String, IntCollection> jclInvertedIndex,
+    																				String column, int opID, String arg) {
+    	Set<String> cardinality = new HashSet<>();
     	Int2ObjectMap<String> filterResult = new Int2ObjectOpenHashMap<>();
     	FilterOperator filtOp = checkOperator(opID);
     	int real_pos = realColumPos(column);
+    	int colPos = dimensionMeta.get(column);
 		for(Entry<Integer, String> e : tuples.entrySet()){
+			if (filterResult.containsKey(e.getKey())) continue;
 			String [] splitArr = e.getValue().split("\\|");
 			String columnVal = splitArr[real_pos];
 			boolean result = filtOp.op(columnVal, arg);
-			if (result)
-				filterResult.put(e.getKey(), e.getValue());
-
+			if (result) {
+				IntCollection intCol = jclInvertedIndex.get("$" + colPos + "$" + columnVal);
+				intCol.forEach(tid -> filterResult.put(tid, e.getValue()));
+				cardinality.add(columnVal);
+//				filterResult.put(e.getKey(), e.getValue());
+			}
 		}
-		return filterResult;
+		int card = cardinality.size();
+
+		if (card == 0) card = 1;
+		Object [] returns = {filterResult, card};
+		return returns;
     }
     
     private Object2ObjectMap<String, IntCollection> generateReult(Int2ObjectMap<String> rawReults, List<String> columns){
@@ -122,6 +159,24 @@ public class Filter
     		cleanTuple.append("|"+splitedTuple[i]);*/
     	
     	return cleanTuple.toString();
+    }
+    
+    int cardilanityInquire(Int2ObjectMap<String> tuples, Set<String> invertedIndexKeySet,String column) {
+    	Set<String> cardinality = new HashSet<>();
+    	int real_pos = realColumPos(column);
+    	for(Entry<Integer,String> e : tuples.entrySet()) {
+    		String [] splitArr = e.getValue().split("\\|");
+			String columnVal = splitArr[real_pos];
+    		cardinality.add(columnVal);
+    	}
+    	
+    	/*int posCol = mesureMeta.get(column);
+    	Set<String> cardinality = invertedIndexKeySet.stream()
+					                	 .filter(s -> s.startsWith("$" + posCol + "$"))
+					                	 .collect(Collectors.toSet());*/
+    	
+    	int card = cardinality.size();
+    	return card;
     }
     
     private int realColumPos(String column) {
@@ -157,5 +212,11 @@ public class Filter
 		default:
 			return null;
 		}
+    }
+    
+    int hash(String s) {
+    	int h = s.hashCode();
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
     }
  }
